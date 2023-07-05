2f39bf90176a0f9799533ba96518bd832dfe5fe1
Leonardo Caracho <leeo.apc@gmail.com>
2023-07-05 20:25:06 -0300

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Leonardo Caracho on 05/07/23.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
