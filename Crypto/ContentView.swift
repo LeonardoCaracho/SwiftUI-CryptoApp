71b6cc60fedf890a236bdadf4a68767988d8e8a4
Leonardo Caracho <leeo.apc@gmail.com>
2023-07-05 20:44:28 -0300

    Add assets and create color scheme

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -9,13 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Accent Color")
+                    .foregroundColor(Color.theme.accent)
+                
+            }
         }
-        .padding()
     }
 }
 
---
+++ Crypto/Extensions/Color.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  Crypto
+//
+//  Created by Leonardo Caracho on 05/07/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondary = Color("SecondaryTextColor")
+}
