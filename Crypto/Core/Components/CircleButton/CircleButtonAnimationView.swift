d4ad748143920999fc7df026de6581cbdbcaccc7
Leonardo Caracho <leeo.apc@gmail.com>
2023-07-06 11:23:18 -0300

    - create home view - add navigation header - create button components

---
+++ Crypto/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,31 @@
+//
+//  CircleButtonAnimationView.swift
+//  Crypto
+//
+//  Created by Leonardo Caracho on 06/07/23.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    @Binding var animate: Bool
+    
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scaleEffect(animate ? 1.0 : 0)
+            .opacity(animate ? 0 : 1)
+            .animation(.easeOut(duration: 1), value: animate)
+            .onAppear {
+                animate.toggle()
+            }
+    }
+}
+
+struct CircleButtonAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonAnimationView(animate: .constant(false))
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100)
+    }
+}
