82fbf7fc165d21e29bdcc4db4b64afae55beecf7
Leonardo Caracho <leeo.apc@gmail.com>
2023-07-19 21:33:10 -0300

    feat: save images on local cache

--- Crypto/Services/CoinImageService.swift
+++ Crypto/Services/CoinImageService.swift
@@ -11,15 +11,31 @@ import Combine
 
 class CoinImageService {
     @Published var image: UIImage? = nil
+    
     private var imageSubscription: AnyCancellable?
     private let coin: Coin
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
     
     init(coin: Coin) {
         self.coin = coin
+        self.imageName = coin.id
+        
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: coin.id, folderName: folderName) {
+            image = savedImage
+            print("Retrieved image from file manager!")
+        } else {
+            downloadCoinImage()
+            print("Donwloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
         guard let url = URL(
             string: coin.image
         ) else { return }
@@ -31,8 +47,10 @@ class CoinImageService {
             .sink(
                 receiveCompletion: NetWorkingManager.handleCompletion,
                 receiveValue: { [weak self] returnedImage in
-                    self?.image = returnedImage
-                    self?.imageSubscription?.cancel()
+                    guard let self = self, let downloadedImage = returnedImage else { return }
+                    self.image = downloadedImage
+                    self.imageSubscription?.cancel()
+                    self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
                 })
     }
 }
